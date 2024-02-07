63471871ae9b7ad7905b386c0c557dc5a5eb7896
Ansariel <ansariel.hiller@phoenixviewer.com>
2024-02-07 18:10:38 +0100

    Update to 2.02.20

--- build-cmd.sh
+++ build-cmd.sh
@@ -2,8 +2,8 @@
 
 FMOD_DOWNLOAD_BASE="http://erebus/dev/pkg/"
 FMOD_ROOT_NAME="fmodstudioapi"
-FMOD_VERSION="20218"
-FMOD_VERSION_PRETTY="2.02.18"
+FMOD_VERSION="20220"
+FMOD_VERSION_PRETTY="2.02.20"
 
 cd "$(dirname "$0")"
 
