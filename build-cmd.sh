11a9c87f74b6205651eba026690e9f8308571782
heuguchon <heuguchon@wiselight.kr>
2024-08-10 18:19:02 +0900

    Update to 2.02.23

--- build-cmd.sh
+++ build-cmd.sh
@@ -2,8 +2,8 @@
 
 FMOD_DOWNLOAD_BASE="http://erebus/dev/pkg/"
 FMOD_ROOT_NAME="fmodstudioapi"
-FMOD_VERSION="20220"
-FMOD_VERSION_PRETTY="2.02.20"
+FMOD_VERSION="20223"
+FMOD_VERSION_PRETTY="2.02.23"
 
 cd "$(dirname "$0")"
 
